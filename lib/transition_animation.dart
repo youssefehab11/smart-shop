f0f4398892e3821fbf1487eb0acb393e887edf01
youssefehab11 <90460729+youssefehab11@users.noreply.github.com>
2023-04-13 18:18:33 +0200

    Add files via upload

--- lib/transition_animation.dart
+++ lib/transition_animation.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:graduationproject/shopping_list.dart';
-
 class ScaleAnimationRoute extends PageRouteBuilder {
   final Page;
   ScaleAnimationRoute({this.Page})
@@ -25,6 +23,22 @@ class SlideRightAnimationRoute extends PageRouteBuilder {
             transitionDuration: const Duration(milliseconds: 500),
             transitionsBuilder: (context, animation, animationtwo, child) {
               var begin = const Offset(1.0 , 0.0);
+              var end = const Offset(0.0 , 0.0);
+              var curvesAnimation =
+                  CurvedAnimation(parent: animation, curve: Curves.easeIn);
+              var tween = Tween(begin: begin, end: end);
+              return SlideTransition(
+                  position: tween.animate(curvesAnimation), child: child);
+            });
+}
+class SlideDownAnimationRoute extends PageRouteBuilder {
+  final Page;
+  SlideDownAnimationRoute({this.Page})
+      : super(
+            pageBuilder: (context, animation, secondaryAnimation) => Page,
+            transitionDuration: const Duration(milliseconds: 500),
+            transitionsBuilder: (context, animation, animationtwo, child) {
+              var begin = const Offset(0.0 , 1.0);
               var end = const Offset(0.0 , 0.0);
               var curvesAnimation =
                   CurvedAnimation(parent: animation, curve: Curves.easeIn);
